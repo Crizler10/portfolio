827b7244f9ab826e3ed3d119a682e88195fdef6b
Joyal PJ <145261595+Crizler10@users.noreply.github.com>
2023-11-05 14:47:41 +0530

    Add files via upload

---
+++ contact.js
@@ -0,0 +1,10 @@
+ScrollReveal({
+    /*reset: true,8*/
+    distance: '80px',
+    duration: 2000,
+    delay:200
+});
+ScrollReveal().reveal('.section-title', { origin: 'top' });
+ScrollReveal().reveal('.c1', { origin: 'left' });
+ScrollReveal().reveal('.c3', { origin: 'right' });
+ScrollReveal().reveal('.c2', { origin: 'bottom' });
